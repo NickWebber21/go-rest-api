e8c2e00158a5000e21e05fe4d307f40bfcd93219
nick webber <nicholaswebbercse@outlook.com>
2026-04-25 11:44:47 -0400

    assign the handler function to an endpoint path

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 // album struct declaration
 // store album data in memory
 type album struct {
@@ -14,4 +16,16 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) { // gin.Context very important part of Gin it carries requestt details, validates and serializes JSON, n more
+	c.IndentedJSON(http.StatusOK, albums)
 }
